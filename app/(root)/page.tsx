6989c13fe7394be2682ab3329cf6b59bc485854f
pandeemiC <lkooxer55@hotmail.com>
2025-05-24 16:22:57 +0300

    refactored path added pattern and card setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,4 @@
-import SearchForm from "@/app/components/SearchForm";
+import SearchForm from "@/components/SearchForm";
 
 export default async function Home({
   searchParams,
@@ -6,16 +6,45 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: "Yesterday",
+      views: 55,
+      author: {
+        _id: 1,
+      },
+      _id: 1,
+      description: "Simple description",
+      image:
+        "https://images.unsplash.com/photo-1552820728-8b83bb6b773f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Gaming",
+      title: "Pro Gaming",
+    },
+  ];
 
   return (
     <>
-      <section className="secondary_container">
-        <h1 className="heading">Express & Share</h1>
-        <p className="sub-heading !max-w-3xl">
+      <section className="pattern secondary_container">
+        <h1 className="heading shadow-xl">Express & Share</h1>
+        <p className="sub-heading bg-background-100 p-2 rounded-md shadow-md !max-w-3xl">
           Your Voice, Your Blog, Live on the Feed
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-black font-sansation font-black text-3xl">
+          {query ? `Search results for "${query}"` : "All Blogs"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {/* {posts?.length > 0 ? (
+            posts.map((post: BlogCardType, i: number) => <BlogCard />)
+          ) : (
+            <p className="no-result">No blogs found..</p>
+          )} */}
+        </ul>
       </section>
     </>
   );
--- components/SearchFormReset.tsx
+++ components/SearchFormReset.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { X } from "lucide-react";
 
 const SearchFormReset = () => {
   const reset = () => {
@@ -12,7 +13,7 @@ const SearchFormReset = () => {
   return (
     <button type="reset" onClick={reset}>
       <Link href="/" className="search-btn text-white">
-        X
+        <X className="size-5" />
       </Link>
     </button>
   );
