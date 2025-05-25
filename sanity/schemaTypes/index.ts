1594409237efca6d1b58ce97bbdb57144725e09e
pandeemiC <lkooxer55@hotmail.com>
2025-05-25 20:31:56 +0300

    sanity setup, added two schemas (author, blog) and created a dummy post by a dummyauthor

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { author } from "@/sanity/schemaTypes/author";
+import { blog } from "@/sanity/schemaTypes/blog";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [author, blog],
+};
