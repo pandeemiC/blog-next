960acf9aa802fb696240963d4c1b8734d44c4713
pandeemiC <lkooxer55@hotmail.com>
2025-05-28 15:55:49 +0300

    shadcn components and style updates

--- components/BlogForm.tsx
+++ components/BlogForm.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import React from "react";
+import { Input } from "./ui/input";
 
 const BlogForm = () => {
-  return <div>BlogForm</div>;
+  return (
+    <form action={() => {}} className="blog-form">
+      <div className="">
+        <label htmlFor="title" className="blog-form-label">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          className="blog-form-input"
+          required
+          placeholder="Blog Title"
+        />
+      </div>
+    </form>
+  );
 };
 
 export default BlogForm;
