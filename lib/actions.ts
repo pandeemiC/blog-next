0e2ebba1a1de7081efb54f732a18b0654783aacf
pandeemiC <lkooxer55@hotmail.com>
2025-05-29 15:47:38 +0300

    getting necessary params, extract session and values and write to sanity to return

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { auth } from "@/auth";
 import { parseServerActionResp } from "./utils";
+import slugify from "slugify";
+import { writeClient } from "@/sanity/lib/write-client";
 
 export const createEntry = async (
   state: any,
@@ -16,4 +18,34 @@ export const createEntry = async (
   const { title, description, category, link } = Object.fromEntries(
     Array.from(form).filter(([key]) => key !== "article")
   );
+
+  const slug = slugify(title as string, { lower: true, strict: true });
+
+  try {
+    const blog = {
+      title,
+      description,
+      category,
+      image: link,
+      slug: {
+        _type: slug,
+        current: slug,
+      },
+      author: {
+        _type: "reference",
+        _ref: session?.id,
+      },
+      article,
+    };
+
+    const result = await writeClient.create({ _type: "blog", ...blog });
+    return parseServerActionResp({ ...result, error: "", status: "SUCCESS" });
+  } catch (error) {
+    console.log(error);
+
+    return parseServerActionResp({
+      error: JSON.stringify(error),
+      status: "ERROR",
+    });
+  }
 };
